23bdb165dc71f49b43330edfe9c401b1fe8667a2
Gurasshrimp <jack04309487@gmail.com>
2024-04-05 20:43:03 +0800

    更新timestamp

--- cogs/earthquake.py
+++ cogs/earthquake.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from datetime import datetime
+import sqlite3
 
 class Earthquake(commands.GroupCog):
     def __init__(self,bot: commands.Bot):
@@ -14,6 +15,7 @@ class Earthquake(commands.GroupCog):
         self.apikey = os.getenv('WEATHERAPI')
         self.url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001"
         self.params = {"Authorization":self.apikey,"limit":"1"}
+        self.number = 0
     
     @app_commands.command(name="query",description="查詢近期地震警示")
     @app_commands.describe(回傳資料筆數="最大可請求數量為10")
@@ -34,68 +36,44 @@ class Earthquake(commands.GroupCog):
         data = requests.get(url=self.url,params=self.params).json()['records']["Earthquake"]
         embeds = []
 
-        try:
-            for i in range(len(data)):
-                image = data[i]["ReportImageURI"]
-                url = data[i]["Web"]
-                info = data[i]["EarthquakeInfo"]
+       
+        for i in range(len(data)):
+            image = data[i]["ReportImageURI"]
+            url = data[i]["Web"]
+            info = data[i]["EarthquakeInfo"]
 
-                dateandtime = datetime.fromisoformat(info["OriginTime"])
-                date = dateandtime.strftime("%Y/%m/%d")
-                time = dateandtime.strftime("%X")
-                depth = info["FocalDepth"]
-                location = info["Epicenter"]["Location"]
-                magnitude = info["EarthquakeMagnitude"]["MagnitudeValue"]
-                
-                if data[i]["ReportColor"] == "綠色":
-                    color = discord.Color.green()
-                elif info["ReportColor"] == "黃色":
-                    color = discord.Color.yellow()
-                elif info["ReportColor"] == "橘色":
-                    color = discord.Color.orange()
-                else:
-                    color = discord.Color.red()
-                
-                embed = discord.Embed(
-                    title="**地震報告**",
-                    color=color,
-                    description=url
-                    )
-                        
-                embed.add_field(name="時間",value=f"{date}\n{time}",inline=True)
-                embed.add_field(name="震央",value=location,inline=False)
-                embed.add_field(name="震源深度",value=f"{depth}公里",inline=True)
-                embed.add_field(name="芮氏規模",value=magnitude,inline=True)
-                embed.set_image(url=image)
+            dateandtime = datetime.fromisoformat(info["OriginTime"])
+            date = dateandtime.strftime("%Y/%m/%d")
+            time = dateandtime.strftime("%X")
+            depth = info["FocalDepth"]
+            location = info["Epicenter"]["Location"]
+            magnitude = info["EarthquakeMagnitude"]["MagnitudeValue"]
+            
+            if data[i]["ReportColor"] == "綠色":
+                color = discord.Color.green()
+            elif info["ReportColor"] == "黃色":
+                color = discord.Color.yellow()
+            elif info["ReportColor"] == "橘色":
+                color = discord.Color.orange()
+            else:
+                color = discord.Color.red()
+            
+            embed = discord.Embed(
+                title="**地震報告**",
+                color=color,
+                description=url
+                )
+                    
+            embed.add_field(name="時間",value=f"{date}\n{time}",inline=True)
+            embed.add_field(name="震央",value=location,inline=False)
+            embed.add_field(name="震源深度",value=f"{depth}公里",inline=True)
+            embed.add_field(name="芮氏規模",value=magnitude,inline=True)
+            embed.set_image(url=image)
+            embed.timestamp = dateandtime
 
-                embeds.append(embed)
+            embeds.append(embed)
         
-                
-            # url = data["ReportImageURI"]
-            # title = data["ReportContent"]
-
-            # value = data["EarthquakeInfo"]["EarthquakeMagnitude"]["MagnitudeValue"]
-
-            # if value <= 3:
-            #     color = discord.Colour.green()
-            # elif value <= 5:
-            #     color = discord.Colour.yellow()
-            # else:
-            #     color = discord.Colour.red()
-
-            # embed = discord.Embed(
-            #     title=title,
-            #     color = color,
-            #     )
-            # embed.set_image(url=url)
-            for embed in embeds:
-
-                await interaction.followup.send(embeds=embeds)
-        except Exception as e:
-            print(e)
-        
-        
-    
+            await interaction.followup.send(embeds=embeds)
 
 async def setup(bot):
     await bot.add_cog(Earthquake(bot))
